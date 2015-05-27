d4c46390b690414040dc8b95891464c5cfa838f1
Lennart Edberg <lennartedb@hotmail.se>
2015-05-27 14:18:07 +0200

    Första Javascript TEST

---
+++ js/game.js
@@ -0,0 +1,99 @@
+var button1 = document.getElementById("btn1");
+var button2 = document.getElementById("btn2");
+var button3 = document.getElementById("btn3");
+var button4 = document.getElementById("btn4");
+var answer = "";
+var score;
+var questionNumber = 0;
+var questionList = ["", "", "", "", ""];
+
+
+button1.addEventListener("click", function()
+{
+   if(answer = button1.value)
+   {
+        score++;
+       questionNumber++;
+   }
+    else
+    {
+        questionNumber++;
+    }
+    
+})
+
+
+button2.addEventListener("click", function()
+{
+   if(answer = button2.value)
+   {
+        score++;
+       questionNumber++;
+   }
+    else
+    {
+        questionNumber++;
+    }
+    
+})
+
+
+button3.addEventListener("click", function()
+{
+   if(answer = button3.value)
+   {
+        score++;
+       questionNumber++;
+   }
+    else
+    {
+        questionNumber++;
+    }
+    
+})
+
+
+button4.addEventListener("click", function()
+{
+   if(answer = button4.value)
+   {
+        score++;
+       questionNumber++;
+   }
+    else
+    {
+        questionNumber++;
+    }
+    
+})
+
+
+if(questionNumber = 0)
+{
+    answer = "TobbeP";
+    button1.value = "TobbeP";
+    button2.value = "Fiske";
+    button3.value = "Hejhej";
+    button4.value = "CsNoob";
+    document.getElementById("question").value = "Test";
+}
+
+if(questionNumber = 1)
+{
+   answer = "TobbeP2";
+}
+
+if(questionNumber = 2)
+{
+   answer = "TobbeP3";
+}
+
+if(questionNumber = 3)
+{
+   answer = "TobbeP4";
+}
+
+if(questionNumber = 4)
+{
+   answer = "TobbeP5";
+}
